fea90c9510b2a284055e5dad9a5a43e750373d13
KitWong2048 <hjie.huang@gmail.com>
2020-10-13 17:11:05 +0800

    'hhj'

--- utils/losses_r.py
+++ utils/losses_r.py
@@ -513,8 +513,9 @@ class CE_DiceLoss_r(nn.Module):
         self.smooth = smooth
         self.dice = GDiceLossV2()
         self.cross_entropy = nn.CrossEntropyLoss(weight=None, reduction=reduction, ignore_index=ignore_index)
-    
+        self.weight = weight
+        
     def forward(self, output, target):
-        CE_loss = self.cross_entropy(output, target)*weight[0]
-        dice_loss = self.dice(output, target)*weight[1]
-        return (CE_loss + dice_loss) / (weight[0] + weight[1])
+        CE_loss = self.cross_entropy(output, target)*self.weight[0]
+        dice_loss = self.dice(output, target)*self.weight[1]
+        return (CE_loss + dice_loss) / (self.weight[0] + self.weight[1])
